4c5e4066c07e18b96413fd617f3c4747a4667f90
AkionAG07 <akion.cheng.jimenez@est.una.ac.cr>
2024-05-01 21:21:22 -0600

    Add ButtonCancel  and acept component

---
+++ src/components/ButtomCancel.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ButtomCancel() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ButtomCancel
---
+++ src/components/ButtonAcept.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ButtonAcept({Title} : {Title: string}) {
+   
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ButtonAcept
