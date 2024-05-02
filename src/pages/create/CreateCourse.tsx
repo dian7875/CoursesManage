554bdb7f50273e2eff1291461193c5b9205e3a47
BrandonJafeth <brandon.carrillo.alvarez@est.una.ac.cr>
2024-05-01 20:57:43 -0600

    Actualizar estilos del formulario de creación de cursos

--- src/pages/create/CreateCourse.tsx
+++ src/pages/create/CreateCourse.tsx
@@ -9,14 +9,14 @@ function CreateCourse() {
 
   const onSubmit = async (data: any) => {
     try {
-  
+
       const spaceAvailable = Math.floor(Math.random() * (Number(data.maximun_quota) + 1));
-  
+
       const formData = {
         ...data,
         space_available: spaceAvailable,
       };
-  
+
       await createCourse(formData);
     } catch (error) {
       console.error('Error in create course', error);
@@ -38,64 +38,60 @@ function CreateCourse() {
   return (
 
     <>
-     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet"></link>
-     <div className="container">
-
-     <h1 className='add-course'>Add Course</h1>
-
- 
-
-      <form className='Form-Class' onSubmit={handleSubmit(onSubmit)}>
-     
-     
-     
-      <div className="input-group">
-        <label htmlFor="name">Course Name</label>
-        <input type="text" id='name'  {...register('name')} />
-        <label htmlFor="professor">Professors Name</label>
-        <input type="text" id='professor' {...register('professor')} />
+      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet"></link>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400&display=swap" rel="stylesheet"></link>
 
 
-   
+      <div className="container">
 
-        </div>
-
-
-   
-        <div className="input-group">
+        <h1 className='add-course'>Add course</h1>
 
 
-        <label htmlFor="course_code"> Course Code</label>
-        <input type="text" id='course_code'  {...register('course_code')} />
-        
-        <label htmlFor="classroom_number">Classroom Number</label>
-        <input type="number" id='classroom_number'  {...register('classroom_number')} />
 
-     
-        
-        </div>
-
-        <div className="input-group">
-        <label htmlFor="status">Course Status</label>
-        <input type="checkbox" id='status' {...register('status')} />
-
-        </div>
-        
-        <div className="input-group-maximum">
-        <label htmlFor="maximun_quota">Maximum Quota</label>
-        <input type="number" id='maximun_quota'  {...register('maximun_quota')} />
-
-        </div>
+        <form className='Form-Class' onSubmit={handleSubmit(onSubmit)}>
 
 
-        <div className="button-group">
-        <button className='cancelButton' type='button' onClick={onCancel}>Cancel</button>
-        <button className='sendButton' type="submit">Save</button>
-        
-        </div>
 
-      </form>
-    </div>
+          <div className="input-group input-field">
+            <label htmlFor="name">Course Name</label>
+            <input type="text" id='name'  {...register('name')} />
+          </div>
+
+          <div className="input-group input-field">
+            <label htmlFor="professor">Teacher’s Name</label>
+            <input type="text" id='professor' {...register('professor')} />
+
+          </div>
+
+          <div className="input-group input-field">
+            <label htmlFor="course_code"> Course Code</label>
+            <input type="text" id='course_code'  {...register('course_code')} />
+            
+          </div>
+          <div className="input-group input-field">
+            <label htmlFor="classroom_number">Classroom Number</label>
+            <input type="number" id='classroom_number'  {...register('classroom_number')} />
+          </div>
+
+          <div className="input-group input-field">
+            <label htmlFor="status">Course Status</label>
+            <input type="checkbox" id='status' {...register('status')} />
+          </div>
+
+          <div className="input-group input-field">
+            <label htmlFor="maximun_quota">Maximum Quota</label>
+            <input type="number" id='maximun_quota'  {...register('maximun_quota')} />
+          </div>
+
+
+          <div className="button-group">
+            <button className='cancelButton' type='button' onClick={onCancel}>Cancel</button>
+            <button className='sendButton' type="submit">Save</button>
+
+          </div>
+
+        </form>
+      </div>
 
     </>
   )
