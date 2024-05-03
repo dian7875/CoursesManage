5e302edc96f190bab9b471a6c8b168be38389bd7
AkionAG07 <akion.cheng.jimenez@est.una.ac.cr>
2024-05-02 21:41:10 -0600

    chore: Update npm dependencies and add SweetAlert2 library

--- src/components/ButtonsForms.tsx
+++ src/components/ButtonsForms.tsx
@@ -1,10 +1,29 @@
 import "./ButtonsForms.css";
-
+import Swal from "sweetalert2";
      export const ButtonAcept = ({Title} : {Title: string}) => {
-   
+      const handleAlert = () => {
+        Swal.fire({
+          title: "Are you sure about to do this action?",
+          showCancelButton: true,
+          confirmButtonText: "Yes",
+          cancelButtonText: "No",
+          customClass: {
+          cancelButton: 'custom-cancel-button',
+          confirmButton: 'custom-confirm-button'
+          }
+        }).then((result) => {
+          /* Read more about isConfirmed, isDenied below */
+          if (result.isConfirmed) {
+            Swal.fire("Saved!", "", "success");
+          } else if (result.isDenied) {
+            Swal.fire("Changes are not saved", "", "info");
+          }
+        });
+      }
+
         return (
           <>
-            <button className='sendButton' type='submit'>{Title}</button>
+            <button className='sendButton' onClick={handleAlert} type='submit'>{Title}</button>
           </>
         )
       }
