143aab7bb5df38f74f9643d605a8c4dc1851cbcf
Dian <dian7875.a@gmail.com>
2024-05-02 20:39:20 -0600

    For test

--- src/Hooks/useEditCourse.ts
+++ src/Hooks/useEditCourse.ts
@@ -60,7 +60,7 @@ const useEditCourse = (courseId: string) => {
           data.maximun_quota = parseInt(data.maximun_quota) || 0;
         }
         await editCourse({data});
-        navigate('/');
+        alert("SIUUUUU");
       } catch (error) {
         console.error('Error al editar el curso', error);
       }
