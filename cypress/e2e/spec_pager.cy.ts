ceec467b403aa889a2c2e26f6020b714d8087008
Dian <dian7875.a@gmail.com>
2024-05-08 17:29:14 -0600

    Arreglo de nombres

--- cypress/e2e/spec_pager.cy.ts
+++ cypress/e2e/spec_pager.cy.ts
@@ -1,33 +1,33 @@
 describe('Page Test', () => {
-  it('passes', () => {
+  it('Page Load', () => {
     cy.visit('http://localhost:5173/');
   });
 
-  it('should exist and be clickable', () => {
+  it('Prev and Next Page', () => {
     cy.visit('http://localhost:5173/');
     cy.get('button.Btn[title="prev"]').click();
     cy.get('button.Btn[title="next"]').should('be.visible').click();
 
   });
 
-  it('Test Delete an course',()=>{
+  it('Delete course',()=>{
     cy.visit('http://localhost:5173/');
     cy.get('[title="delete"]').should('be.visible').eq(0).click()
     //cy.get('button.swal2-confirm').click() //Confirmacion del delet
   
   })
-  it('Test Change Dark mode',()=>{
+  it('Change Dark mode',()=>{
     cy.visit('http://localhost:5173/');
     cy.get('span.slider-theme').click()
     cy.get('span.slider-theme').click()
   
   })
-  it('Test View Course',()=>{
+  it('View Course',()=>{
     cy.visit('http://localhost:5173/');
     cy.get('[title="view"]').should('be.visible').eq(0).click();
     cy.get('button.cancelButton').click();
   })
-  it('Test change Limit',()=>{
+  it('Change Limit',()=>{
     cy.visit('http://localhost:5173/');
     cy.get('select.LimitSel').select('10');
     cy.get('select.LimitSel').should('have.value', '10');
